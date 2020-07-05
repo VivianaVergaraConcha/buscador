0452dda890d3b181ee535e9a5954d37355339d56
Viviana <viviana.vergara.concha@gmail.com>
2020-07-05 18:32:53 -0400

    Forms and use api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios'
+
+//MATERIAL
+import { TextField, Container, Grid} from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import AddIcon from '@material-ui/icons/Add';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [nombre, setNombre] = useState('');
+
+  function handleChange(event){
+    setNombre(event.target.value);
+  }
+  
+  function handleClick(nombre){
+    axios.get(`https://swapi.dev/api/people/?search=${nombre}`)
+        .then(response => {
+          console.log(response.data);
+          setNombre('');
+        })
+        .catch(
+          error => alert(error)
+        )
+  }
+
+  return(
+    <Container fixed>
+     <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <TextField label="Buscar" onChange={handleChange} value={nombre}/>
+        <IconButton type="submit" aria-label="search" color="primary" onClick={() => handleClick(nombre)}>
+          <SearchIcon />
+        </IconButton>
+      </Grid>
+      <Grid item xs={3}>
+          <Card> 
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                nombre
+              </Typography>
+              <Typography color="textSecondary">
+                <span>•</span>pelo
+                <br/>
+                <span>•</span>ojos
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton color="primary" aria-label="add">
+                <AddIcon/>
+              </IconButton>
+            </CardActions>
+          </Card>  
+        </Grid>
+     </Grid>
+    </Container>
   );
 }
 
